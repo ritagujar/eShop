a9627a0cb94236476c274638ab854c517c571aae
Rita-Gujar <ritagujar14@gmail.com>
2021-05-03 02:08:44 +0530

    Shopping cart functionality

--- frontend/src/actions/cartAtions.js
+++ frontend/src/actions/cartAtions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -19,5 +19,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   //After dispatching saving it to localstorage
   // JSON.stringify bcz we can ony save strings in localStorage
   // and once we  take it out use JSON.pare to parse it back to js (store.js)
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
